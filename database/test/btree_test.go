099224ac656caa0870620efa0bf3c84b680ce9ab
VincentBai-dotcom <butcheryyy@gmail.com>
2024-12-31 02:27:13 -0800

    add test code for read operation

--- database/test/btree_test.go
+++ database/test/btree_test.go
@@ -5,11 +5,27 @@ import (
 	"testing"
 )
 
-func TestReadMissingValue(t *testing.T) {
-	bTreeController := btree.NewC()
+func TestBtreeRead(t *testing.T) {
+	c := btree.NewC()
+	c.Add("1", "1")
+	c.Add("2", "2")
 
-	_, ok := bTreeController.Read("fdsa")
+	val, ok := c.Read("1")
+
+	if !ok {
+		t.Error("Read fail")
+	}
+
+	if val != c.Ref["1"] {
+		t.Errorf("Read fail: expected %v, got %v", c.Ref["1"], val)
+	}
+}
+
+func TestReadMissingKey(t *testing.T) {
+	c := btree.NewC()
+
+	val, ok := c.Read("fdsa")
 	if ok {
-		print("ok")
+		t.Errorf("read missing key and got value %s", val)
 	}
 }
